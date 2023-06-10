36c01d49ae6816b5977940713ffdca27ca51da0d
GSY <gle722@yandex.ru>
2023-06-10 10:44:08 +0300

    Решили задачу 2

---
+++ Lesson_2/Task_2.java
@@ -0,0 +1,53 @@
+
+/*
+ * Напишите метод, который сжимает строку.
+ * Пример: вход aaaabbbcddaa
+ * Ответ: резульат - a4b3cd2a2
+ */
+
+import java.util.Scanner;
+
+public class Task_2
+{
+    public static void main(String[] args)
+    {
+        String text = input_symbol();
+        text = ZipString(text);
+        System.out.println(text);
+    }
+    public static String input_symbol()
+    {
+        Scanner sc = new Scanner(System.in, "cp866");
+        System.out.println("Введите текст: ");
+        String result = sc.nextLine();
+        return result;
+    }
+    public static String ZipString(String str)
+    {
+        StringBuilder result = new StringBuilder();
+        result.append(str.charAt(0));
+        int count = 1;
+        for (int i = 1; i < str.length(); i++) 
+        {
+            if (str.charAt(i - 1) == str.charAt(i))
+            {
+                count += 1;
+            }
+            else
+            {
+                if (count != 1)
+                {
+                    result.append(""+count);
+                    count = 1;
+                    result.append(str.charAt(i));
+                }
+                else
+                {
+                    result.append(str.charAt(i));
+                }
+            }
+        } 
+        result.append("" + count);
+        return result.toString();
+    }
+}
